6ffb1dcc75149c07c6b47b391f65521ffa371d13
Daniel J. Pritchett <dpritchett@gmail.com>
2015-03-27 16:18:57 -0500

    Cleaner go

--- main.go
+++ main.go
@@ -22,6 +22,20 @@ func getSpheroFd() string {
 
 func main() {
 	bot := webby.NewWebby(getSpheroFd())
+
+	bot.AddCommand("green", func() { bot.Driver.SetRGB(0, 255, 0) }, "turning green")
+	bot.AddCommand("red", func() { bot.Driver.SetRGB(255, 0, 0) }, "turning red")
+	bot.AddCommand("blue", func() { bot.Driver.SetRGB(0, 0, 255) }, "turning blue")
+
+	bot.AddCommand("forward", func() { bot.Roll(75, 0) }, "moving forward")
+	bot.AddCommand("right", func() { bot.Roll(75, 90) }, "moving right")
+	bot.AddCommand("back", func() { bot.Roll(75, 180) }, "moving back")
+	bot.AddCommand("left", func() { bot.Roll(75, 270) }, "moving left")
+
+	bot.AddCommand("wiggle", bot.Wiggle, "wiggle wiggle wiggle wiggle wiggle wiggle wiggle!")
+
+	bot.AddCommand("new_color", bot.SetRandColor, "new color!")
+
 	bot.HostFileAtRoute("./static/buttons.html", "/demo/")
 	log.Fatal(bot.Start())
 }
--- webby/webby.go
+++ webby/webby.go
@@ -5,7 +5,6 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	"time"
 
 	"github.com/hybridgroup/gobot"
 	"github.com/hybridgroup/gobot/api"
@@ -15,7 +14,8 @@ import (
 type Webby struct {
 	Gobot     *gobot.Gobot
 	Sphero    *gobot.Robot
-	ApiServer *api.API
+	APIServer *api.API
+	Driver    *sphero.SpheroDriver
 }
 
 func NewWebby(spheroFd string) *Webby {
@@ -24,79 +24,26 @@ func NewWebby(spheroFd string) *Webby {
 	ss.Gobot = gobot.NewGobot()
 
 	adaptor := sphero.NewSpheroAdaptor("sphero", spheroFd)
-	driver := sphero.NewSpheroDriver(adaptor, "sphero")
+	ss.Driver = sphero.NewSpheroDriver(adaptor, "sphero")
 
 	spheroid := gobot.NewRobot("sphero",
 		[]gobot.Connection{adaptor},
-		[]gobot.Device{driver},
+		[]gobot.Device{ss.Driver},
 	)
 
 	ss.Sphero = ss.Gobot.AddRobot(spheroid)
 
-	// Accessible via http://localhost:3000/robots/sphero/commands/turn_blue
-	ss.Sphero.AddCommand("blue", func(params map[string]interface{}) interface{} {
-		driver.SetRGB(0, 0, 255)
-		return "turning blue"
-	})
-
-	ss.Sphero.AddCommand("green", func(params map[string]interface{}) interface{} {
-		driver.SetRGB(0, 255, 0)
-		return "turning green"
-	})
-
-	ss.Sphero.AddCommand("red", func(params map[string]interface{}) interface{} {
-		driver.SetRGB(255, 0, 0)
-		return "turning red"
-	})
-
-	ss.Sphero.AddCommand("left", func(params map[string]interface{}) interface{} {
-		driver.Roll(75, uint16(270))
-		return "moving left"
-	})
-
-	ss.Sphero.AddCommand("right", func(params map[string]interface{}) interface{} {
-		driver.Roll(75, uint16(90))
-		return "moving right"
-	})
-
-	ss.Sphero.AddCommand("forward", func(params map[string]interface{}) interface{} {
-		driver.Roll(75, uint16(0))
-		return "moving forward"
-	})
-
-	ss.Sphero.AddCommand("back", func(params map[string]interface{}) interface{} {
-		driver.Roll(75, uint16(180))
-		return "moving back"
-	})
-
-	rgb := func() uint8 {
-		return uint8(gobot.Rand(255))
-	}
-
-	setRandColor := func() {
-		driver.SetRGB(rgb(), rgb(), rgb())
-	}
-
-	wiggle := func() {
-		setRandColor()
-		driver.Roll(75, uint16(gobot.Rand(360)))
-	}
-
-	ss.Sphero.AddCommand("wiggle", func(params map[string]interface{}) interface{} {
-		times := []time.Duration{0, 2, 4}
-
-		for _, t := range times {
-			gobot.After(t*time.Second, wiggle)
-		}
-
-		return "wiggle wiggle wiggle wiggle wiggle wiggle wiggle"
-	})
-
 	// Starts the API server on default port 3000
-	ss.ApiServer = api.NewAPI(ss.Gobot)
-	ss.ApiServer.Start()
+	ss.APIServer = api.NewAPI(ss.Gobot)
 
 	return ss
+}
+
+func (ss *Webby) AddCommand(name string, impl func(), desc string) {
+	ss.Sphero.AddCommand(name, func(params map[string]interface{}) interface{} {
+		impl()
+		return desc
+	})
 }
 
 func (ss *Webby) HostFileAtRoute(filePath, route string) {
@@ -110,9 +57,32 @@ func (ss *Webby) HostFileAtRoute(filePath, route string) {
 		io.WriteString(w, string(pageHTML))
 	}
 
-	ss.ApiServer.Get(route, demoFileService)
+	ss.APIServer.Get(route, demoFileService)
 }
 
 func (ss *Webby) Start() (errs []error) {
+	ss.APIServer.Start()
 	return ss.Gobot.Start()
+}
+
+func (ss *Webby) SetRandColor() {
+	rgb := func() uint8 {
+		return uint8(gobot.Rand(255))
+	}
+
+	ss.Driver.SetRGB(rgb(), rgb(), rgb())
+}
+
+func (ss *Webby) Wiggle() {
+	ss.SetRandColor()
+	ss.Roll(50+ss.Rand(50), ss.Rand(360))
+}
+
+// exposing some useful sphero utils to use for new commands
+func (ss *Webby) Rand(n int) int {
+	return gobot.Rand(n)
+}
+
+func (ss *Webby) Roll(distance, heading int) {
+	ss.Driver.Roll(uint8(distance), uint16(heading))
 }
